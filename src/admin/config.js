3a3549d2f695907fa5494f2075c550b4d7b04742
Gleb <glebstrunnikov@gmail.com>
2025-09-06 18:17:05 +0300

    config

---
+++ src/admin/config.js
@@ -0,0 +1,15 @@
+  GNU nano 7.2                                             vite.config.js                                                      const { mergeConfig } = require('vite');
+
+module.exports = (config) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+    server: {
+      allowedHosts: true
+    },
+  });
+};
