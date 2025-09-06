90e0ceabac86a2c75741969c40608ea713f3e241
Gleb <glebstrunnikov@gmail.com>
2025-09-06 22:50:36 +0300

    change middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,24 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000", "https://pmem-europa.de"],
+      credentials: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      jsonLimit: "100mb",
+      formLimit: "100mb",
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
