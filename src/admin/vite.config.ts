99d8ac76d812daa63bf7eb9ee9ec837f67123397
Gleb <glebstrunnikov@gmail.com>
2025-09-06 18:29:25 +0300

    fixed stupidity? 2

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -1,4 +1,6 @@
-module.exports = (config) => {
+import { mergeConfig, type UserConfig } from "vite";
+
+export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
     resolve: {
